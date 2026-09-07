d69b13114e5fa6898e133e05d7e5d28c79b1e116
Diganth <diganthhm714@gmail.com>
2026-09-07 23:54:45 +0530

    connected to mongodb

--- Backend/database/mongodb.go
+++ Backend/database/mongodb.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const connectionURL = "mongodb://localhost:27017"
+const dbName = "library"
+const colName = "student"
+
+var Collection *mongo.Collection
+
+func init() {
+	clientOptions := options.Client().ApplyURI(connectionURL)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	Collection = client.Database(dbName).Collection(colName)
+}
